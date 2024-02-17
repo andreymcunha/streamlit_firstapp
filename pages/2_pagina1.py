71b0503c48b212a287656f4444c00102543743b8
andreymcunha <160057366+andreymcunha@users.noreply.github.com>
2024-02-16 21:01:13 -0300

    Update 2_pagina1.pye

--- pages/2_pagina1.py
+++ pages/2_pagina1.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
 
 #Textos
 #faz os elementos ocuparem todo o espaço da tela. OBS.: Set_page_config só pode ser chamado uma vez por página de app e precisa ser a primeira linha de comando no script
@@ -22,64 +19,3 @@ else:
     st.header(f"Essa página rodou {st.session_state.counter} vezes.")
 st.button("Clica aqui rapidão")
 
-#se eu quero que o meu texto esteja dentro da barra de navegação, incluir "sidebar" entre st e text
-st.sidebar.header('Meu texto')
-
-#markdown le o # como um código para dar destaque ao texto que vem a seguir (markdown é uma linguagem)
-#st.markdown('# Meu título')
-
-#representa significância para SEO e dá menos destaque ao texto
-#st.caption('Minha legenda')
-
-pessoas = [{'Nome': 'Caio', 'Idade':22},
-            {'Nome': 'Marcos', 'Idade':33}]
-#write é mais genérico, serve para apresentar dados misturados
-#st.write("# Pessoas", pessoas)
-
-#exibição de dados
-df = pd.DataFrame(
-    np.random.rand(10,3),
-    columns=['Preço','Taxa de ocupação','Taxa de inadimplência']
-)
-#diferenças entre table e dataframe. o dataframe permite alterações da tabela pelo usuário ao navegar
-#st.dataframe(df)
-#st.table(df)
-
-df_pessoas = pd.DataFrame(pessoas)
-
-#elementos gráficos
-filtro = st.selectbox('Filtro da tabela', ['Preço', 'Taxa de ocupação', 'Taxa de inadimplência'])
-col1, col2, col3 = st.columns(3)
-
-# Adicionando conteúdo às colunas
-with col1:
-    st.dataframe(df_pessoas)
-
-with col2:
-    st.line_chart(df)
-
-with col3:
-    st.bar_chart(df_pessoas,x='Nome',y='Idade')
-
-# Exibindo o filtro selecionado
-st.write(f'Você selecionou o filtro: {filtro}')
-
-#st.line_chart(df)
-#st.bar_chart(df)
-#if st.sidebar.button('Exibir opções'):
-    #if st.button('Gráfico de linhas'):
-        #st.line_chart(df)
-    #if st.button('Gráfico de barras'):
-        #st.bar_chart(df)
-
-exibir_opcoes = st.sidebar.checkbox('Exibir opções')
-if exibir_opcoes:
-    opcao_grafico = st.selectbox('Selecione o tipo de gráfico', ['Gráfico de linhas', 'Gráfico de barras', 'Gráfico de pizza'])
-
-    if opcao_grafico == 'Gráfico de linhas':
-        st.line_chart(df)
-    elif opcao_grafico == 'Gráfico de barras':
-        st.bar_chart(df)
-    elif opcao_grafico == 'Gráfico de pizza':
-            fig = px.scatter(df,x='Preço', y='Taxa de ocupação', title='Gráfico de plotly')
-            st.plotly_chart(fig)
